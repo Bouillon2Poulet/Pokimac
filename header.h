a15824c6e97a6911ac9343ce7f9f918cbc7da246
Rom1 <romain.serres74@gmail.com>
2022-01-14 09:26:15 +0100

    -Carte -Pokemon -Choix starter

--- header.h
+++ header.h
@@ -3,25 +3,22 @@ using namespace std;
 
 //Vérification de l'OS pour le clear
 #ifdef WIN32
-    #define clear() system("cls")
+        #define clear() system("cls")
 #else
-    #ifdef UNIX
-        #define clear() system("clear")
-    #else
- 
+    #define clear() system("clear")
+
         #ifndef NBLIGCLS
             #define NBLIGCLS 30
         #endif
- 
-        void clear()
+
+        /*id clear()
         {
             int i;
             for(i = 0; i < NBLIGCLS; i++)
             {
                 printf("\n");
             }
-        }
-    #endif
+        }*/
 #endif
 ///////
 
@@ -29,16 +26,49 @@ using namespace std;
 const int width = 10;
 const int height = 10;
 
+struct Inventaire{
+    int nb_potions;
+    int nb_pokeball;
+    int nb_popo2;
+    int nb_trucopif;
+};
+
+struct Pokemon{
+    string nom;
+    string type;
+    int pv;
+    int dmg;
+    int def;
+};
+
 struct Player{
     int posx;
     int posy;
+    string pseudo;
+    Inventaire inv;
+    Pokemon ekip[6];
 };
+
+//Liste des pokémons
+Pokemon listePkm [2]; //Déclaration de la liste
+
+extern Pokemon salameche;  // Salamèche
+extern Pokemon bulbizarre;   //Bulbizarre
+
 
 
 // le prototypage
+void copyPokemon(Pokemon source, Pokemon *destination);
+void init_pokemons(Pokemon listePkm[]);
+void intro (Player *player,string *suivant);
+void choix_starter(Player *player, string *suivant, Pokemon listePkm[]);
+void intro2(Player *player, string *suivant);
+
 void affiche_map (char map[width*height]);
-void deplacement_perso(Player *player);
+void deplacement_perso(Player *player, char input);
 void initPlayer (Player *player, int posx, int posy);
 void updateMap(char map[], Player player);
+void affiche_menu(const Player player);
+void init_inv(Inventaire inv);
+void affiche_inventaire(Inventaire inv);
 
-    
--- test_map.cpp
+++ test_map.cpp
@@ -2,56 +2,163 @@
 #include "header.h"
 using namespace std;
 
-int main(){
-    
-    Player player;
+Pokemon salameche;
+
+int main()
+{
+    string suivant;
+
+    Player player; // Initialise les coordonnées du joueur et son inventaire
     player.posx = 5;
     player.posy = 5;
+    init_inv(player.inv);
+    Pokemon listePkm [2];
+    init_pokemons(listePkm); //Initialise les pokémons
+
+    /////////////////////////LE DEBUT DU JEU ////////////////////////////////
+
+    intro(&player, &suivant);
+    choix_starter(&player, &suivant, listePkm);
+    intro2(&player, &suivant);
+/////////////////// LA MAP + DEPLACEMENT PERSO //////////////////////////
 
     //déf de la map
     char map [width*height];
-    
+
     // remplissage de la map
     for (int i=0; i<width*height;i++){
         map[i] = '0';
     }
-    
+
     // affichage de la map
     affiche_map (map);
-    
+
 
     // test du déplacement
-    while (true){
-        deplacement_perso(&player);
-        updateMap(map, player);
-        //cout << "en x : " << player.posx << " // en y : " << player.posy << endl;
-    }
+    while(true){
+        char input;
+        cin >> input;
+        clear();
+        if (input=='1') { // on entre dans l'inventaire;
+            affiche_inventaire(player.inv);
+            cout << "Appuiez sur ECHAP pour revenir sur la map" << endl;
+            cin >> suivant;
+            clear();
+        }
 
+
+        deplacement_perso(&player, input);
+        updateMap(map, player);
+        affiche_menu(player);
+    }
     return 0;
 }
 
 
-///////////////// LES FONCTIONS !!!!
+
+    /////////////////////////Définitions fonctions////////////////////////////////
+
+void intro (Player *player, string *suivant)
+{
+    cout << "Bienvenue dans le monde des Pokemons..." << endl << endl;
+    cout << "...Appuyez sur entrer pour commencer l'aventure"<< endl;
+    *suivant = getchar();
+    clear();
+
+    cout << "Comment t'appelles-tu ?" << endl;
+    cin >> player->pseudo; // Pseudo
+    clear();
+
+    getchar(); // Vider le tampon du premier getchar()
+
+    cout << "OK " << player->pseudo << " tu te crois capable de pouvoir être le meilleur dresseur" << endl;
+    cout << " d'animaux de combats que l'on nomme POKEMON ?" << endl;
+    cout << "si oui appuie sur entrer avec vigueur !!!!"<< endl;
+    *suivant = getchar();
+    clear();
+}
+
+void intro2(Player *player, string *suivant)
+{
+    cout << "INTRO 2";
+}
+
+void init_pokemons(Pokemon listePkm[])
+{
+    salameche.nom = "Salameche";
+    salameche.type = "Feu";
+    salameche.pv = 39;
+    salameche.dmg = 60;
+    salameche.def = 60;
+    listePkm[0] = salameche;
+
+    bulbizarre.nom = "Bulbizarre";
+    bulbizarre.type = "Plante";
+    bulbizarre.pv = 39;
+    bulbizarre.dmg = 60;
+    bulbizarre.def = 60;
+    listePkm[0] = bulbizarre;
+}
+
+void copyPokemon(Pokemon source, Pokemon *destination) //Copie un pokémon source vers un pokémon cible pour résoudre le problème des strings qui se copient mal
+{
+    destination->nom=source.nom;
+    destination->type=source.type;
+    destination->pv=source.pv;
+    destination->dmg=source.dmg;
+    destination->def=source.def;
+}
+
+
+void choix_starter (Player *player, string *suivant, Pokemon listePkm[])
+{
+    clear();
+    cout << "Choix du Pokemon :" << endl;
+    cout << "\033[31m 1 - Salamèche\033[0m\n";
+    cout << "\033[32m 2 - Bulbizarre\033[0m\n";
+    cout << "\033[34m 3 - Carapuce\033[0m\n";
+    int choix;
+    cin >> choix;
+    switch (choix)
+    {
+        case 1: copyPokemon(listePkm[0],&player->ekip[0]);
+        cout << "pkm = " << player->ekip[0].nom << endl;
+        break;
+
+        case 2: copyPokemon(listePkm[1],&player->ekip[0]);
+        cout << "pkm = " << player->ekip[0].nom << endl;
+        break;
+
+        case 3: copyPokemon(listePkm[2],&player->ekip[0]);
+        cout << "pkm = " << player->ekip[0].nom << endl;
+        break;
+
+        default: cout << "Veuillez entrer un chiffre entre 1 et 3" << endl;
+        *suivant = getchar();
+        choix_starter(player,suivant,listePkm);
+    }
+}
+
+
+
 void affiche_map (char map[width*height]){
     for (int k=0; k<height; k++){
         for (int i = 0; i < width; i++){
             cout << map[i];
         }
-        cout << endl; 
+        cout << endl;
     }
 }
 
-void deplacement_perso(Player *player){
+void deplacement_perso(Player *player, char input){
     // demander le déplacement
-    char input;
-    cin >> input;
-    clear();
+    //if (input == 'p') return input; // on arrête de joueur à la map en appuyant sur P
 
-    
+
     // déplacement selon l'input et selon la position
     switch (input){
         case 'z':
-            if (player->posx  >0) player->posy --;
+            if (player->posy >0) player->posy --;
         break;
 
         case 'q':
@@ -66,11 +173,12 @@ void deplacement_perso(Player *player){
             if (player->posx <width-1) player->posx ++;
         break;
     }
+    //return ' '; // renvoie espace si le joueur joue
 }
 
 void updateMap(char map[], Player player)
 {
-
+    int nb_aff_ligne = 0;
     for (int j=0; j<height; j++)
         {
             for (int h=0; h<width; h++)
@@ -78,13 +186,41 @@ void updateMap(char map[], Player player)
                 if (h==player.posx && j==player.posy)
                 {
                     cout << 1;
+                    nb_aff_ligne ++;
                     h++;
                 }
-            
-            cout << map[h];
+                nb_aff_ligne ++;
+                if (nb_aff_ligne<=width){
+                    cout << map[h];
+                }
             }
+        cout << "        " << nb_aff_ligne;
         cout << endl;
+        nb_aff_ligne =0;
         }
 }
 
 
+void affiche_menu(const Player player){
+    cout << endl << "----------" << endl << endl;
+    cout << "1 : Inventaire" << endl;
+    cout << "2 : Pokémons" << endl;
+    cout << "3 : "<< player.pseudo << endl;
+}
+
+void init_inv(Inventaire inv){
+    inv.nb_pokeball = 0;
+    inv.nb_popo2 = 0;
+    inv.nb_potions = 0;
+    inv.nb_trucopif = 0;
+}
+
+void affiche_inventaire(Inventaire inv){
+    cout << "- INVENTAIRE -" << endl<<endl;
+    cout << "mmmmmh yeah !! " << endl;
+    cout << "ton nombre de pokeballs est : " << inv.nb_pokeball << endl;
+    cout << "ton nombre de popo2 est : " << inv.nb_popo2 << endl;
+    cout << "ton nombre de trucopif est : " << inv.nb_trucopif << endl << endl << endl;
+}
+
+//void affiche_pokemon ()
